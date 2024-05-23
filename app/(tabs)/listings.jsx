8196e2fc96bb8efba90b82fc17a9f9c4a4088da3
Samy <cherif.samy1000@gmail.com>
2024-05-23 22:08:59 +0100

    Added loading placeholder

--- app/(tabs)/listings.jsx
+++ app/(tabs)/listings.jsx
@@ -1,9 +1,10 @@
-import React, {useLayoutEffect, useState} from 'react'
+import React, {useEffect, useLayoutEffect, useState} from 'react'
 import {FlatList, LayoutAnimation, Platform, UIManager} from 'react-native'
 import DynamicSafeAreaView from "../../components/DynamicSafeAreaView";
 import defaultCities from "../../constants/defaultCities";
 import TravelCard from "../../components/TravelCard";
 import ListingForm from "../../components/ListingForm";
+import SkeletonTravelCard from "../../components/SkeletonTravelCard";
 
 const Listings = () => {
   const [origin, setOrigin] = useState(null)
@@ -19,6 +20,7 @@ const Listings = () => {
   const [date, setDate] = useState(today)
 
   const [formVisible, setFormVisible] = useState(true);
+  const [loading, setLoading] = useState(true)
 
   // enable animation for android
   if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
@@ -32,68 +34,76 @@ const Listings = () => {
 
   useLayoutEffect(() => LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut), [formVisible])
 
-  const data = [{
-    id: "1234",
-    origin: "Safi",
-    destination: "Rabat",
-    date: "25/05/2024",
-    weight: 20,
-    price: 5,
-    name: "samy",
-    rating: 5,
-    image: require("../test.png")
-  }, {
-    id: "12345",
-    origin: "Safi",
-    destination: "Rabat",
-    date: "25/05/2024",
-    weight: 20,
-    price: 5,
-    name: "samy",
-    rating: 3.5,
-    image: require("../test.png")
-  }, {
-    id: "123fr45",
-    origin: "Safi",
-    destination: "Rabat",
-    date: "25/05/2024",
-    weight: 20,
-    price: 5,
-    name: "samy",
-    rating: 0,
-    image: require("../test.png")
-  }, {
-    id: "12frfr345",
-    origin: "Safi",
-    destination: "Rabat",
-    date: "25/05/2024",
-    weight: 20,
-    price: 5,
-    name: "samy",
-    rating: 3.5,
-    image: require("../test.png")
-  }, {
-    id: "123frfr45",
-    origin: "Safi",
-    destination: "Rabat",
-    date: "25/05/2024",
-    weight: 20,
-    price: 5,
-    name: "samy",
-    rating: 3.5,
-    image: require("../test.png")
-  }, {
-    id: "1233fe45",
-    origin: "Safi",
-    destination: "Rabat",
-    date: "25/05/2024",
-    weight: 20,
-    price: 5,
-    name: "samy",
-    rating: 3.5,
-    image: require("../test.png")
-  },]
-
+  const [data, setData] = useState([])
+  useEffect(() => {
+    // Simulate a network request
+    setTimeout(() => {
+      setData([
+        {
+          id: "1234",
+          origin: "Safi",
+          destination: "Rabat",
+          date: "25/05/2024",
+          weight: 20,
+          price: 5,
+          name: "samy",
+          rating: 5,
+          image: require("../test.png")
+        },
+        {
+          id: "12345",
+          origin: "Safi",
+          destination: "Rabat",
+          date: "25/05/2024",
+          weight: 20,
+          price: 5,
+          name: "samy",
+          rating: 3.5,
+          image: require("../test.png")
+        }, {
+          id: "123fr45",
+          origin: "Safi",
+          destination: "Rabat",
+          date: "25/05/2024",
+          weight: 20,
+          price: 5,
+          name: "samy",
+          rating: 0,
+          image: require("../test.png")
+        }, {
+          id: "12frfr345",
+          origin: "Safi",
+          destination: "Rabat",
+          date: "25/05/2024",
+          weight: 20,
+          price: 5,
+          name: "samy",
+          rating: 3.5,
+          image: require("../test.png")
+        }, {
+          id: "123frfr45",
+          origin: "Safi",
+          destination: "Rabat",
+          date: "25/05/2024",
+          weight: 20,
+          price: 5,
+          name: "samy",
+          rating: 3.5,
+          image: require("../test.png")
+        }, {
+          id: "1233fe45",
+          origin: "Safi",
+          destination: "Rabat",
+          date: "25/05/2024",
+          weight: 20,
+          price: 5,
+          name: "samy",
+          rating: 3.5,
+          image: require("../test.png")
+        },]);
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (<DynamicSafeAreaView className="h-full bg-white">
     <ListingForm today={today} setDestination={setDestination} setOrigin={setOrigin}
@@ -101,11 +111,12 @@ const Listings = () => {
                  originData={originData} formVisible={formVisible} setOpen={setOpen} setDate={setDate} open={open}
                  date={date} destination={destination} origin={origin} setWeight={setWeight} weight={weight}/>
 
-    <FlatList
-        data={data}
-        renderItem={(item) => TravelCard(item)}
-        onScroll={handleScroll}
-        keyExtractor={item => item.id}/>
+
+    {loading &&
+        <FlatList data={[...Array(6).keys()]} renderItem={SkeletonTravelCard} keyExtractor={(item) => item.toString()}
+                  scrollEnabled={false}/>}
+    {!loading && <FlatList data={data} renderItem={(item) => TravelCard(item)} onScroll={handleScroll}
+                           keyExtractor={item => item.id}/>}
   </DynamicSafeAreaView>)
 }
 
---
+++ components/SkeletonTravelCard.jsx
@@ -0,0 +1,46 @@
+import {View} from "react-native";
+import {Card} from "react-native-elements";
+import COLORS from "../constants/colors";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+import React from "react";
+import {Skeleton} from "@rneui/base";
+
+const SkeletonTravelCard = () => {
+  return (<View className="w-full">
+    <Card containerStyle={{
+      borderRadius: 10, borderWidth: 1, borderColor: COLORS.cgrey,
+    }}>
+
+      <View className="flex-row items-center justify-between mb-4">
+        <Skeleton width="40%" height={25}/>
+        <MaterialCommunityIcons style={{marginHorizontal: 10, width: "10%"}} name="sign-direction" size={40}
+                                color={COLORS.brand}/>
+        <Skeleton width="40%" height={25}/>
+
+      </View>
+
+      <View className="flex-row mb-2 ">
+        <Skeleton width="50%" height={15}/>
+      </View>
+
+      <View className="flex-row items-center justify-between mb-2">
+        <Skeleton width="40%" height={15}/>
+        <Skeleton width="40%" height={15}/>
+
+      </View>
+      <Card.Divider color={COLORS.brand} width={1}/>
+      <View className="flex-row justify-between ">
+        <Skeleton width={55} height={55} circle={true}/>
+        <View className="flex-col ml-4 justify-between">
+          <Skeleton width={80} height={15}/>
+          <Skeleton width={120} height={30}/>
+        </View>
+        <View className="flex-row items-center justify-center">
+          <Skeleton width={80} height={30}/>
+        </View>
+      </View>
+    </Card>
+  </View>)
+}
+
+export default SkeletonTravelCard
